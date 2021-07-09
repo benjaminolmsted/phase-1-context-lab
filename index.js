ac7dbf021f3cd5802007df365508954c9e75c50f
benjamin <benjamin.olmsted@gmail.com>
2021-07-09 10:23:52 -0700

    commit tree 2a7de087ba1544c0d6a8471a1b56ca1501ddf055

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
 /* Your Code Here */
+ function createEmployeeRecord(recordArray){
+     return {
+         firstName: recordArray[0],
+         familyName: recordArray[1],
+         title: recordArray[2],
+         payPerHour: recordArray[3],
+         timeInEvents: [],
+         timeOutEvents: []
+     }
+ }
+
+ function createEmployeeRecords(empArray){
+     return empArray.map((employee) =>{
+        return createEmployeeRecord(employee)
+     })
+ }
+
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const inTimeEvent = this.timeInEvents.find(event => event.date === date )
+    const outTimeEvent = this.timeOutEvents.find(event => event.date === date)
+    return (outTimeEvent.hour - inTimeEvent.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    return this.payPerHour * hoursWorkedOnDate.call(this, date)
+}
+
+function findEmployeeByFirstName(scrArray, name){
+    let employee;
+    scrArray.forEach( emp => {
+        if(emp.firstName === name){
+            employee = emp;
+        }
+    })
+    return employee
+}
+
+function calculatePayroll(empRecords){
+    return empRecords.reduce((acc, emp) =>{
+        return acc + allWagesFor.call(emp)
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
